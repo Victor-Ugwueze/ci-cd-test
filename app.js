7def8fb5bb5a3d8c2e8a477138ebf391e4daf04c
Victor Ugwueze <victorugwueze@gmail.com>
2021-06-16 01:02:01 +0100

    change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express  = require('express')
 const app = express();
 
 
-app.get('*', (req, res) => res.send('Hello'))
+app.get('*', (req, res) => res.send('I am a changed person'))
 
 
 app.listen(process.env.PORT || 3000, () => console.log('App started running...'))
